0f8bda3d94dedd79bb444391e777ec713cfc3a5b
Srinivasan Mohan <srinivas@onepwr.org>
2014-09-15 19:43:36 -0700

    Recursive find to fetch whole tree. Need to improve this.

--- lib/worker.rb
+++ lib/worker.rb
@@ -9,7 +9,7 @@ module Reconfig
     include EM::Deferrable
 
     def initialize(opts=Hash.new)
-      #$stderr.puts "INIT: "+JSON.pretty_generate(opts)
+      $stderr.puts "INIT: "+JSON.pretty_generate(opts)
       @key=opts["key"]
       @id=opts["id"]
       @ckey=opts["id"] + "(Key: #{@key})"
@@ -19,26 +19,48 @@ module Reconfig
       @reload=opts["reloadcmd"]
       @check=opts["checkcmd"] 
       @client=opts["client"] 
-      @debug=opts["debug"]    
+      @debug=opts["debug"] 
+      @nr=opts.has_key?("notreally") && opts["notreally"] ? true : false   
     end
 
     def debug!
       @debug=!@debug
     end
 
+    #This stupidity to fetch a full tree when a recursive watch triggers.
+    def find(key=nil,mode=false)
+      rethash=Hash.new
+      obj=@client.get(key,recursive: mode)
+      if obj.node.dir
+        childkeys=obj.children.map {|x| x.key }
+        p childkeys
+        if !mode
+          return childkeys
+        else
+          childkeys.each do |x|
+            rethash.merge!(find(x,mode))
+          end 
+        end
+      else
+        rethash[key]=obj.value
+      end
+      rethash
+    end
+
+    #Hmmm... If we are watching a tree recursively, how can we get back a list of keys/values without having to fetch it?
     def run
-      modedesc=@recursive ? "TREE": "LEAF"
+      modedesc=@mode ? "Recursive(Tree)": "SingleKey(Leaf)"
       logmsg("#{@ckey} - Watching #{@key} (#{modedesc})- Change #{@tgt} using #{@src}")
       loop do 
         begin
-       	  val=@client.watch(@key, recursive: @mode)
-          $stderr.puts "#{@ckey} - "
-          p val 
+       	  obj=@client.watch(@key, recursive: @mode)
+          val=find(@key,@mode)
+          logmsg("Key #{@key} changed (Mode=#{modedesc}) => #{val}")
         rescue Exception => e
           logmsg("#{@ckey} - Connectivity error to #{@host}:#{@port} - #{e.inspect}")
           sleep 30
         end 
       end    
     end
-  end
-end
+  end #end of Worker
+end #end of module
